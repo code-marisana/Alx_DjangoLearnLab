750298b83153be6d892df1c3b220441367ea7ff9
code-marisana <marisana@marisanamasha.com>
2025-07-20 16:04:20 +0200

    variables

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,12 @@
 from relationship_app.models import Author, Book, Library, Librarian
-
-author = Author.objects.get(name="Author Name")
+author_name = ''
+library_name = ''
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all()
 
-library = Library.objects.get(name="Library Name")
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
-library = Library.objects.get(name="Library Name")
+library = Library.objects.get(name=library_name)
 librarian_lib = Librarian.objects.get(library=library)
 
